0f4845aa0b655a689cb07d30713ece407c6a5454
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-10-12 20:21:05 -0300

    Fixed navbar menu problem showing disappear on first load

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -5,13 +5,15 @@ import useWidth from '../hooks/useWidth';
 
 const NavbarMenu = () =>{
     const [menu,setMenu] = useState(false);
+    const [firstLoad,setFirstLoad] = useState(true);
     const handleClick = ()=>{
         setMenu(!menu)
+        setFirstLoad(false)
     }
     let currentWidth = useWidth()
     return(
         <nav className='navbar__links'>
-            <button className='navbar__menu-icon' onClick={handleClick}>
+            <button className='navbar__menu-icon' onClick={()=>handleClick()}>
                 <div className='navbar__button-container'>
                     <img src={menu?cancel:menuIcon} alt="Deploy menu icon" />
                 </div>
@@ -19,7 +21,9 @@ const NavbarMenu = () =>{
             <div className={currentWidth<768?
              menu?
                     'navbar__links--appear'
-                    :'navbar__links--empty'
+                    :firstLoad?
+                        'navbar__links--empty'
+                        :'navbar__links--disappear'
             :'navbar__links--row'
             }>
                 <a href="#about" onClick={()=>handleClick()}>SOBRE MI</a>
